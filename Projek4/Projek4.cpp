f9c14ca2b2d1afd4ee28ebf216369ddb50a75089
Mihaly24 <fairuzk20@gmail.com>
2024-04-02 18:52:01 +0700

    Menambahkan detail alamat projek 4

--- Projek4/Projek4.cpp
+++ Projek4/Projek4.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
