8bf9afb97c0b21a5df5f5d7505645aad0200eba8
Mihaly24 <fairuzk20@gmail.com>
2024-04-02 17:54:58 +0700

    Menambahkan struktur pada projek 1

--- Struct/Struct.cpp
+++ Struct/Struct.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	string umur;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+ 
 }
