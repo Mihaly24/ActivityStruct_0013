bae424d76c5348c8daaa6a790da1c2906d152d11
Mihaly24 <fairuzk20@gmail.com>
2024-04-02 19:03:48 +0700

    Memperbaiki kesalahan pada projek 4

--- Projek4/Projek4.cpp
+++ Projek4/Projek4.cpp
@@ -18,6 +18,7 @@ int main()
 {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
+		cout << "Data ke-" << (i + 1) << endl;
 		cout << "Nomor Mahasiswa : ";
 		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
@@ -30,15 +31,19 @@ int main()
 		cin >> mhs[i].alamat.kota;
 		cout << "Umur Mahasiswa : ";
 		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
 	}
 
 	for (int i = 0; i < 3; i++) {
 		cout << endl;
+		cout << "data Mahasiswa ke-" << (i + 1) << endl;
 		cout << "\n NIM : " << mhs[i].nim;
 		cout << "\n Nama : " << mhs[i].nama;
 		cout << "\n Alamat : ";
 		cout << "\n \t Desa : " << mhs[i].alamat.desa;
 		cout << "\n \t Kota: " << mhs[i].alamat.kota;
 		cout << "\n Umur : " << mhs[i].umur;
+		cout << endl;
 	}
 }
