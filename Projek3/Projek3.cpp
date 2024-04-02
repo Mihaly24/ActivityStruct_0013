99569ef3f35235e2ce4f61006bd640473218b931
Mihaly24 <fairuzk20@gmail.com>
2024-04-02 18:48:25 +0700

    Menambahkan include string pada projek 3

--- Projek3/Projek3.cpp
+++ Projek3/Projek3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct DetailAlamat {
@@ -17,9 +18,9 @@ int main()
 {
 	Mahasiswa mhs;
 	cout << "Nomor Mahasiswa : ";
-	getline(cin, mhs.nim);
+	getline(cin ,mhs.nim);
 	cout << "Nama Mahasiswa : ";
-	getline(cin , mhs.nama);
+	getline(cin, mhs.nama);
 
 	cout << "Alamat Mahasiswa : " << endl;
 	cout << "\t Nama Desa : ";
