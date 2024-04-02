1ed62ea87d3f7f1c7a2452bd0840ff6f8294a372
Mihaly24 <fairuzk20@gmail.com>
2024-04-02 18:38:14 +0700

    Mengubah cin menjadi getline cin

--- Projek3/Projek3.cpp
+++ Projek3/Projek3.cpp
@@ -17,7 +17,7 @@ int main()
 {
 	Mahasiswa mhs;
 	cout << "Nomor Mahasiswa : ";
-	cin >> mhs.nim;
+	getline(cin, mhs.nim);
 	cout << "Nama Mahasiswa : ";
-	cin >> mhs.nama;
+	getline(cin , mhs.nama);
 }
